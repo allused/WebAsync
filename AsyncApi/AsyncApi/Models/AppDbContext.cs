98bb40c8a25d2a187e5aa2ba3f91846f7caae4a0
allused <divixed12@gmail.com>
2021-02-15 12:15:36 +0100

    add Product model

---
+++ AsyncApi/AsyncApi/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AsyncApi.Models;
+
+namespace AsyncApi.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ AsyncApi/AsyncApi/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AsyncApi.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
